9fd3f3f2c07423de14988dc965b5bcdf6b94d42a
Rachel Jones <rachelscjones@gmail.com>
2023-08-23 15:20:57 +0100

    created routes for basic CRUD operations

--- server.js
+++ server.js
@@ -1,14 +1,57 @@
-// const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setting up a basic route
 app.get('/', (req, res) => {
   res.send('Welcome to my first RESTful API!');
+});
+
+// Sample data (for demonstration purposes)
+let users = [
+  { id: 1, name: 'Stereotypical Barbie', email: 'barbie@test.com' },
+  { id: 2, name: 'Beach Ken', email: 'ken@test.com' }
+];
+
+// Creating routes for basic CRUD operations
+
+// GET all users
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// GET a specific user by ID
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+// POST a new user
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// PUT (update) an existing user
+app.put('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedUser = req.body;
+  users = users.map(user => (user.id === id ? { ...user, ...updatedUser } : user));
+  res.json(updatedUser);
+});
+
+// DELETE a user
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  users = users.filter(user => user.id !== id);
+  res.json({ message: 'User deleted successfully' });
 });
 
 // Starting the server
